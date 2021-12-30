16f11a710dc41123905bdda2327a76888c56fb2a
Ly Kim Quyen <kim-quyen.ly@nomadic-labs.com>
2021-12-30 11:21:37 +0100

    adding ocamlformat and licence in the code

--- lib/generateProofs.ml
+++ lib/generateProofs.ml
@@ -1,93 +1,169 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021     Marigold <contact@marigold.dev>                    *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
 open Ast
 open Format
 
 exception NotSupportedYet
-exception IncoherentHelper of string 
-let string_of_bop (b : bop) : string = match b with
-| Equality -> "="
-| Inequality -> "<>"
-| Conjonction -> "/\\"
-| Disjonction -> "\\/"
+
+exception IncoherentHelper of string
+
+let string_of_bop (b : bop) : string =
+  match b with
+  | Equality ->
+      "="
+  | Inequality ->
+      "<>"
+  | Conjonction ->
+      "/\\"
+  | Disjonction ->
+      "\\/"
 
 let straight_tactic (fmt : formatter) : unit = fprintf fmt "@[<v 0>crush.@,@]"
 
 let split_tactic (fmt : formatter) : unit = fprintf fmt "@[split.@]@."
 
-let destruct_tactic (fmt : formatter) (var : string): unit = fprintf fmt "@[destruct %s.@]" var
-let induction_tactic (fmt : formatter) (var : string) : unit = fprintf fmt "@[induction %s.@]" var
+let destruct_tactic (fmt : formatter) (var : string) : unit =
+  fprintf fmt "@[destruct %s.@]" var
+
+let induction_tactic (fmt : formatter) (var : string) : unit =
+  fprintf fmt "@[induction %s.@]" var
 
 let nothing (fmt : formatter) : unit = fprintf fmt "@."
+
 let qed (fmt : formatter) : unit = fprintf fmt "@[Qed.@]@."
 
-let arg fmt = function
-  | ASTArg (id,typ) -> fprintf fmt " (%s:%s) " id typ
+let arg fmt = function ASTArg (id, typ) -> fprintf fmt " (%s:%s) " id typ
 
 let semicolon fmt = fprintf fmt ";"
+
 let reduce_axiom fmt axiom = fprintf fmt "@[crush. rewrite %s. crush.@]" axiom
 
 (** [standalone_proof fmt binOp helper] handle the "standalone" proofs 
     which dont need auxiliar lemmas to be written, takes a binary operator and a proof helper to determine
     how print the right coq code.**)
-let standalone_proof fmt b h = match b,h with
-  | Conjonction,Straight -> split_tactic fmt
-  | Disjonction,Left -> fprintf fmt "@[left.@]@."
-  | Disjonction,Right -> fprintf fmt "@[right.@]@."
-  | (Conjonction | Disjonction),_ -> raise (IncoherentHelper "Unusable helper for conjonction/disjonction")
-  | _, Straight -> straight_tactic fmt
-  | _, Case target -> destruct_tactic fmt target; semicolon fmt; straight_tactic fmt
-  | _, Induction target -> induction_tactic fmt target; semicolon fmt; straight_tactic fmt
-  | _ -> raise (IncoherentHelper "left and right are helpers for disjonction only")
+let standalone_proof fmt b h =
+  match (b, h) with
+  | Conjonction, Straight ->
+      split_tactic fmt
+  | Disjonction, Left ->
+      fprintf fmt "@[left.@]@."
+  | Disjonction, Right ->
+      fprintf fmt "@[right.@]@."
+  | (Conjonction | Disjonction), _ ->
+      raise (IncoherentHelper "Unusable helper for conjonction/disjonction")
+  | _, Straight ->
+      straight_tactic fmt
+  | _, Case target ->
+      destruct_tactic fmt target ; semicolon fmt ; straight_tactic fmt
+  | _, Induction target ->
+      induction_tactic fmt target ;
+      semicolon fmt ;
+      straight_tactic fmt
+  | _ ->
+      raise (IncoherentHelper "left and right are helpers for disjonction only")
 
 (** [with_aux_lemmas_proof fmt lemmas binOp helper] handle the proofs 
     which need auxiliar lemmas to be written, takes a list of lemmas (axioms), a binary operator and a proof helper to determine
     how print the right coq code.**)
-let with_aux_lemmas_proof fmt axioms b h = match b,h with
-  | _, Induction target -> induction_tactic fmt target; pp_print_list reduce_axiom fmt axioms
-  | _,_ -> raise NotSupportedYet
+let with_aux_lemmas_proof fmt axioms b h =
+  match (b, h) with
+  | _, Induction target ->
+      induction_tactic fmt target ;
+      pp_print_list reduce_axiom fmt axioms
+  | _, _ ->
+      raise NotSupportedYet
 
 (** [fact_description fmt prop_body] print the body of a "Fact" coq construction with datas contains in a assertion AST.**)
 let fact_description fmt =
   let rec aux fmt = function
-    | ASTAtom (cnt) -> fprintf fmt "%s" cnt
-    | ASTAssert (bop,left,right,_) -> fprintf fmt "@[<v 1>%a %s %a@]" aux left (string_of_bop bop) aux right
-  in aux fmt
+    | ASTAtom cnt ->
+        fprintf fmt "%s" cnt
+    | ASTAssert (bop, left, right, _) ->
+        fprintf fmt "@[<v 1>%a %s %a@]" aux left (string_of_bop bop) aux right
+  in
+  aux fmt
 
 (** [in_assertion fmt prop_body axioms] determine what kind of proof we have to generate, from if we need auxiliars lemmas
     or not .**)
 let in_assertion fmt a axioms =
-  let target = if List.length axioms = 0 then standalone_proof fmt else with_aux_lemmas_proof fmt axioms in
+  let target =
+    if List.length axioms = 0 then standalone_proof fmt
+    else with_aux_lemmas_proof fmt axioms
+  in
   let rec aux = function
-    | ASTAtom (_) -> straight_tactic fmt
-    | ASTAssert(Disjonction,left,_,Left) -> target Disjonction Left; aux left
-    | ASTAssert(Disjonction,_,right,Right) -> target Disjonction Right; aux right
-    | ASTAssert (bop,ASTAtom(_),ASTAtom(_),helper) -> target bop helper
-    | ASTAssert (bop,left,right,helper) -> target bop helper; aux left;aux right
-  in aux a
-
+    | ASTAtom _ ->
+        straight_tactic fmt
+    | ASTAssert (Disjonction, left, _, Left) ->
+        target Disjonction Left ; aux left
+    | ASTAssert (Disjonction, _, right, Right) ->
+        target Disjonction Right ; aux right
+    | ASTAssert (bop, ASTAtom _, ASTAtom _, helper) ->
+        target bop helper
+    | ASTAssert (bop, left, right, helper) ->
+        target bop helper ; aux left ; aux right
+  in
+  aux a
 
 (** [in_property fmt prop] is the function that show the pipeline of an entire property translation**)
 let in_property fmt = function
-  | ASTProp ({assert_name=assert_name';qtf=Some(Forall);args=Some(args');assertt=assertt';lemmas_aux=axioms}) ->
-    fprintf fmt "Fact %s : forall " assert_name';
-    pp_print_list arg fmt args';
-    fprintf fmt "@[, %a.@]@." fact_description assertt';
-    in_assertion fmt assertt' axioms; qed fmt
-  | ASTProp ({assert_name=assert_name';qtf=_;args=None;assertt=assertt';lemmas_aux=axioms}) ->
-    fprintf fmt "Fact %s : " assert_name';
-    fprintf fmt "@[%a.@]@." fact_description assertt';
-    in_assertion fmt assertt' axioms; qed fmt
-  | _ -> raise NotSupportedYet
+  | ASTProp
+      { assert_name= assert_name'
+      ; qtf= Some Forall
+      ; args= Some args'
+      ; assertt= assertt'
+      ; lemmas_aux= axioms } ->
+      fprintf fmt "Fact %s : forall " assert_name' ;
+      pp_print_list arg fmt args' ;
+      fprintf fmt "@[, %a.@]@." fact_description assertt' ;
+      in_assertion fmt assertt' axioms ;
+      qed fmt
+  | ASTProp
+      { assert_name= assert_name'
+      ; qtf= _
+      ; args= None
+      ; assertt= assertt'
+      ; lemmas_aux= axioms } ->
+      fprintf fmt "Fact %s : " assert_name' ;
+      fprintf fmt "@[%a.@]@." fact_description assertt' ;
+      in_assertion fmt assertt' axioms ;
+      qed fmt
+  | _ ->
+      raise NotSupportedYet
 
 let in_block fmt = function
-  | ASTBlock (blockName,props) ->
-    fprintf fmt "@[<v 0>(* Proofs for %s *)@,@]" blockName;
-    pp_print_list in_property fmt props
+  | ASTBlock (blockName, props) ->
+      fprintf fmt "@[<v 0>(* Proofs for %s *)@,@]" blockName ;
+      pp_print_list in_property fmt props
 
 let in_blocks fmt = function
-  | ASTBlocks (properties_blocks) ->
-    fprintf fmt "@[<v 0>From Test Require Import CpdtTactics.@,@]";
-    fprintf fmt "@[<v 0>(* ----PROOFS---- *)@,@]";
-    pp_print_list in_block fmt properties_blocks
+  | ASTBlocks properties_blocks ->
+      fprintf fmt "@[<v 0>From Test Require Import CpdtTactics.@,@]" ;
+      fprintf fmt "@[<v 0>(* ----PROOFS---- *)@,@]" ;
+      pp_print_list in_block fmt properties_blocks
 
-let generate_proof fmt program  =
-  fprintf fmt "%a" in_blocks program;fprintf fmt "@[<v 0> (**END OF PROOFS**)@]@."
+let generate_proof fmt program =
+  fprintf fmt "%a" in_blocks program ;
+  fprintf fmt "@[<v 0> (**END OF PROOFS**)@]@."
--- test/dslprop_test.ml
+++ test/dslprop_test.ml
@@ -1,76 +1,113 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* Open Source License                                                       *)
+(* Copyright (c) 2021     Marigold <contact@marigold.dev>                    *)
+(*                                                                           *)
+(* Permission is hereby granted, free of charge, to any person obtaining a   *)
+(* copy of this software and associated documentation files (the "Software"),*)
+(* to deal in the Software without restriction, including without limitation *)
+(* the rights to use, copy, modify, merge, publish, distribute, sublicense,  *)
+(* and/or sell copies of the Software, and to permit persons to whom the     *)
+(* Software is furnished to do so, subject to the following conditions:      *)
+(*                                                                           *)
+(* The above copyright notice and this permission notice shall be included   *)
+(* in all copies or substantial portions of the Software.                    *)
+(*                                                                           *)
+(* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR*)
+(* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,  *)
+(* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL   *)
+(* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER*)
+(* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING   *)
+(* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER       *)
+(* DEALINGS IN THE SOFTWARE.                                                 *)
+(*                                                                           *)
+(*****************************************************************************)
+
 open Dslprop.DslProp
 open Dslprop.GenerateProofs
 open Format
 
-let bool_and_expected = fprintf std_formatter
-  "From Test Require Import CpdtTactics.
-  (* ----PROOFS---- *)
-  (* Proofs for my_and *)
-  Fact andb_true1 : forall  (b:boolean) , andb Vrai b = b.
-  crush.
-  Qed.
-  Fact andb_true2 : forall  (b:boolean) , andb b Vrai = b.
-  destruct b
-  crush.
-  crush.
-  Qed."
-let nat_trivial_expected = fprintf std_formatter
-  "From Test Require Import CpdtTactics.
-  (* ----PROOFS---- *)
-  (* Proofs for nat *)
-  Fact diff42_41 :  42 <> 41.
-  crush.
-  Qed."
+let bool_and_expected =
+  fprintf std_formatter
+    "From Test Require Import CpdtTactics.\n\
+    \  (* ----PROOFS---- *)\n\
+    \  (* Proofs for my_and *)\n\
+    \  Fact andb_true1 : forall  (b:boolean) , andb Vrai b = b.\n\
+    \  crush.\n\
+    \  Qed.\n\
+    \  Fact andb_true2 : forall  (b:boolean) , andb b Vrai = b.\n\
+    \  destruct b\n\
+    \  crush.\n\
+    \  crush.\n\
+    \  Qed."
 
-let nat_add_expected = fprintf std_formatter
-  "From Test Require Import CpdtTactics.
-  (* ----PROOFS---- *)
-  (* Proofs for add *)
-  Fact add_0 : forall  (m:nat) , add Zero m = m.
-  crush.
-  Qed.
-  Fact add_1 : forall  (n:nat) , add n Zero = n.
-  induction n.
-  crush.
-  crush.
-  Qed."
+let nat_trivial_expected =
+  fprintf std_formatter
+    "From Test Require Import CpdtTactics.\n\
+    \  (* ----PROOFS---- *)\n\
+    \  (* Proofs for nat *)\n\
+    \  Fact diff42_41 :  42 <> 41.\n\
+    \  crush.\n\
+    \  Qed."
+
+let nat_add_expected =
+  fprintf std_formatter
+    "From Test Require Import CpdtTactics.\n\
+    \  (* ----PROOFS---- *)\n\
+    \  (* Proofs for add *)\n\
+    \  Fact add_0 : forall  (m:nat) , add Zero m = m.\n\
+    \  crush.\n\
+    \  Qed.\n\
+    \  Fact add_1 : forall  (n:nat) , add n Zero = n.\n\
+    \  induction n.\n\
+    \  crush.\n\
+    \  crush.\n\
+    \  Qed."
 
 let bool_and_properties =
-  to_proofs [
-    block "andb" [
-      prop "andb_true1" ~context:(forall [("b","boolean")]) 
-        ((((atom "andb b Vrai" =.= atom "b") >> case "b") &^ ((atom "andb Vrai b" =.= atom "b") >> straight)))
-    ]
-  ]
+  to_proofs
+    [ block "andb"
+        [ prop "andb_true1"
+            ~context:(forall [("b", "boolean")])
+            ( atom "andb b Vrai" =.= atom "b" >> case "b"
+            &^ (atom "andb Vrai b" =.= atom "b" >> straight) ) ] ]
 
 let nat_trivial =
-  to_proofs [
-    block "nat" [
-      prop "diff42_41" ((atom "42" =!= atom "41") >> straight)
-    ]
-  ]
+  to_proofs
+    [block "nat" [prop "diff42_41" (atom "42" =!= atom "41" >> straight)]]
 
 let nat_add_properties =
-  to_proofs [
-    block "add" [
-      prop "add_0" ~context:(forall [("m","nat")]) ((atom "add Zero m" =.= atom "m") >> straight);
-      prop "add_1" ~context:(forall [("n","nat")]) ((atom "add n Zero" =.= atom "n") >> induction "n");
-    ]
-  ]
-let test_bool_and () = Alcotest.(check unit) "have to match" bool_and_expected (generate_proof std_formatter bool_and_properties)
-let test_nat_inequal () = Alcotest.(check unit) "have to match" nat_trivial_expected (generate_proof std_formatter nat_trivial)
+  to_proofs
+    [ block "add"
+        [ prop "add_0"
+            ~context:(forall [("m", "nat")])
+            (atom "add Zero m" =.= atom "m" >> straight)
+        ; prop "add_1"
+            ~context:(forall [("n", "nat")])
+            (atom "add n Zero" =.= atom "n" >> induction "n") ] ]
 
-let test_nat_add () = Alcotest.(check unit) "have to match" nat_add_expected (generate_proof std_formatter nat_add_properties)
+let test_bool_and () =
+  Alcotest.(check unit)
+    "have to match" bool_and_expected
+    (generate_proof std_formatter bool_and_properties)
+
+let test_nat_inequal () =
+  Alcotest.(check unit)
+    "have to match" nat_trivial_expected
+    (generate_proof std_formatter nat_trivial)
+
+let test_nat_add () =
+  Alcotest.(check unit)
+    "have to match" nat_add_expected
+    (generate_proof std_formatter nat_add_properties)
 
 let () =
   let open Alcotest in
   run "DSL for express assertions and generate proofs on them"
-  [
-    ("Testing suite for bool/nat properties proofs ",
-      [
-        test_case "Simple straight and case proof on andb" `Quick test_bool_and;
-        test_case "Simple auto proof of an nat inequality" `Quick test_nat_inequal;
-        test_case "Simple straight and inductive proofs for add function on nat" `Quick test_nat_add
-      ]
-    )
-  ]
+    [ ( "Testing suite for bool/nat properties proofs "
+      , [ test_case "Simple straight and case proof on andb" `Quick test_bool_and
+        ; test_case "Simple auto proof of an nat inequality" `Quick
+            test_nat_inequal
+        ; test_case
+            "Simple straight and inductive proofs for add function on nat"
+            `Quick test_nat_add ] ) ]
