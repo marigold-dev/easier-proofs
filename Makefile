5064de6291b06002bddd7a5d9eb6bf759ca59d1d
Ly Kim Quyen <kim-quyen.ly@nomadic-labs.com>
2022-01-04 14:14:24 +0100

    re-organise folder, add ocamlformat, fix readme

--- Makefile
+++ Makefile
@@ -3,3 +3,8 @@ build: clean
 	dune exec test/dslprop_test.exe
 clean:
 	dune clean
+build: clean
+	dune build
+	dune exec test/dslprop_test.exe
+clean:
+	dune clean
--- trash.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for prog in examples/bool/props/*.properties
-do
-    printf "$prog : "
-    leftPartFileName=$(echo $prog | cut -d'.' -f 1)
-    rightPart=".v"
-    res=$(./_build/default/bin/main.exe "$leftPartFileName$rightPart" < $prog)
-done
