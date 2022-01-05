7278f84d752ca0e5737d5e76922fbe37832ec1a0
Ly Kim Quyen <kim-quyen.ly@nomadic-labs.com>
2022-01-05 14:21:06 +0100

    combine coq of ocaml and easier proofs together

--- examples/cleanup.rb
+++ examples/cleanup.rb
@@ -14,11 +14,18 @@ full_path = File.join(easier_proofs_path, examples_path, target_path)
 remove_files =
   Dir.glob(File.join(full_path, "*.v")) 
 for coq_file_name in remove_files.sort do
-    command = "cd #{full_path} && rm -rf #{File.basename(coq_file_name)}"
+    command = "make clean && cd #{full_path} && rm -rf #{File.basename(coq_file_name)}"
+  system(command)
+end
+
+remove_files_aux =
+  Dir.glob(File.join(full_path, ".*.aux")) 
+for aux_file_name in remove_files_aux.sort do
+    command = "cd #{full_path} && rm -rf #{File.basename(aux_file_name)}"
   system(command)
 end
 
 # Remove 
-  system("rm Makefile Makefile.conf _CoqProject")
+  system("rm Makefile Makefile.conf _CoqProject .lia.cache")
 
 
--- examples/nat/trivial.v
+++
@@ -1,6 +0,0 @@
-From Test Require Import CpdtTactics.
-(* ----PROOFS---- *)
-(* Proofs for nat *)
-Fact diff42_41 : 42 <> 41.
-crush.
-Qed.
