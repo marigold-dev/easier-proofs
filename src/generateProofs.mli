4b37f0c1f987e8018fde7767e93d273c5d9976be
Ly Kim Quyen <kim-quyen.ly@nomadic-labs.com>
2022-01-17 09:57:16 +0100

    correct some English

--- src/generateProofs.mli
+++ src/generateProofs.mli
@@ -1,12 +1,12 @@
 
 (** 
-    The core of easier-proof, the file containing all the functions for generating proof from a program written in our DSL
+    The core of easier-proof, the file contains all the functions for generating proof from a program written in our DSL.
 
-    In easier proof, we represente assertion with a DSL, a program in this DSL is a list of blocks, each block concern a precise subject, and in
-    each block we have one of several property we want to prove. We can express an assertion, and we have to give "helpers" with a property
+    In easier-proof, we represent assertion with a DSL, a program in this DSL is a list of blocks, each block concern a precise subject, and in
+    each block we have one of several properties that we want to prove. We can express an assertion, and we have to give "helpers" with a property
     for helping easier-proof to determine how to generate the proof of the assertion.
 
-    Watch {!module:Ast} and {!module:DslProp} for more information about the DSL, the helpers etc....
+    Read in {!module:Ast} and {!module:DslProp} for more information about the DSL, the helpers, etc....
 *)
 
 exception Not_Supported_Yet
@@ -14,14 +14,14 @@ exception Incoherent_Helper of string
 
 val string_of_bop : Ast.bop -> string
 
-(**[straight_tactic fmt] the most basic helper in easier-proof, we use straight when we are sure that the assertion we
-    want to prove is solvable with one chlipala's crush.*)
+(**[straight_tactic fmt] is the most basic helper in easier-proof, we use straight when we are sure that the assertion we
+    want to prove is solvable with the tactic crush of Chlipala.*)
 val straight_tactic : Format.formatter -> unit
 
-(**[split_tactic fmt] print the induction split, needed if we encounter a conjonction operator in our assertion*)
+(**[split_tactic fmt] print the induction split, needed if we encounter a conjunction operator in our assertion.*)
 val split_tactic : Format.formatter -> unit
 
-(**[destruct_tactic fmt var] print the destruct tactic on var, needed for a case reasoning on our proof*)
+(**[destruct_tactic fmt var] print the destruct tactic on var, needed for a case reasoning on our proof.*)
 val destruct_tactic : Format.formatter -> string -> unit
 
 (**[induction_tactic fmt var] print the induction tactic on var*)
@@ -36,8 +36,8 @@ val arg : Format.formatter -> Ast.arg -> unit
 (**[semicolon] print the ltac command ";"*)
 val semicolon : Format.formatter -> unit
 
-(**[dot fmt] print a '.', we don't necesseraly use the dot after a coq tactic since we can use the ltac command ";" 
-    in order to automate a part of the proof*)
+(**[dot fmt] print a '.', we don't necesseraly use the dot after a Coq tactic since we can use the ltac command ";" 
+    in order to automate a part of the proof.*)
 val dot : Format.formatter -> unit
 
 (**[hint_rewrite fmt name] generate a hint for a rewrite needed in the generated proof*)
@@ -53,11 +53,11 @@ val standalone_proof : Format.formatter -> Ast.bop -> Ast.helper -> unit
 val fact_description : Format.formatter -> Ast.prop_body -> unit
 
 (** [in_assertion fmt prop_body hints] determine what kind of proof we have to generate,
-    and use the hints if there is some.*)
+    and use the hints if there is any.*)
 val in_assertion :
     Format.formatter -> Ast.prop_body -> string list -> unit
 
-(** [in_property fmt prop] is the function that show the pipeline of an entire proof generation for a given assertion*)
+(** [in_property fmt prop] is a function that show the pipeline of an entire proof generation for a given assertion.*)
 val in_property : Format.formatter -> Ast.prop -> unit
 
 val in_block : Format.formatter -> Ast.block -> unit
