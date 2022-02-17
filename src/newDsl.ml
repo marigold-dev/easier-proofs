19c7b50ba6e4f84b1c792d00422a03c387ff1f13
Hakimba <hakim.baaloudj@gmail.com>
2022-02-17 11:19:50 +0100

    starting implementation of the new DSL

---
+++ src/newDsl.ml
@@ -0,0 +1,19 @@
+module DL = struct
+  type _ t =
+    | [] : string t
+    | ( :: ) : ('a * 'b t) -> ('a -> 'b) t
+end
+
+type quantifier = Forall
+type _ binding = Binding : (string * string) -> 'a binding
+type ('a, 'b) quantified_binding = quantifier * ('a binding -> 'b) DL.t
+
+type ('a, 'b) fact =
+  { name : string
+  ; quantifier : ('a, 'b) quantified_binding
+  ; block : 'a binding -> 'b
+  }
+
+let nat : string -> [ `Nat ] binding = fun x -> Binding ("nat", x)
+let forall l = Forall, l
+let fact name quantifier block = { name; quantifier; block }
