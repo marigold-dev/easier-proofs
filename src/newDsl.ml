3fb088e8ec4013a22c3c235ccb04136d07838326
Hakimba <hakim.baaloudj@gmail.com>
2022-03-03 11:13:01 +0100

    some tries with a new dsl

--- src/newDsl.ml
+++ src/newDsl.ml
@@ -18,12 +18,11 @@ let nat : string -> [ `Nat ] binding = fun x -> Binding ("nat", x)
 let forall l = Forall, l
 let fact name quantifier block = { name; quantifier; block }
 
-
 (**exemples of terms*)
 
 let t =
-  fact 
-     ("add_right_zero") 
+  fact
+     ("add_right_zero")
      (forall DL.[ nat "x"; nat "y"; nat "z" ])
      (fun _x _y _z -> "")
 ;;
